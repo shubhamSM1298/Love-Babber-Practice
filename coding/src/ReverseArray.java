07a55652aef91c7d86952072c9f82d948f860d66
Shubham Mallya <91719093+shubhamSM1298@users.noreply.github.com>
2022-09-01 19:42:33 +0530

    reverse Array

--- coding/src/ReverseArray.java
+++ coding/src/ReverseArray.java
@@ -1,6 +1,34 @@
+import java.util.*;
 public class ReverseArray {
-    public static void main(String[] args)
+    public static void main(String[] args) {
+        Scanner q = new Scanner(System.in);
+        int n = q.nextInt();
+        int a[] = new int[n];
+        for (int i = 0; i < n; i++)
+            a[i] = q.nextInt();
+reverse(a);
+
+    }
+
+    public static void reverse(int[] a)
     {
-        Scanner q =new Scanner
+        int s=0, end=a.length-1,temp;
+        while(s<end)
+        {
+            temp =a[s];
+            a[s]=a[end];
+            a[end]=temp;
+
+            s++;
+            end--;
+        }
+        printArray(a);
+    }
+    public static void printArray(int a[])
+    {
+        for(int i=0;i<a.length;i++)
+        {
+            System.out.print(a[i]+" ");
+        }
     }
 }
